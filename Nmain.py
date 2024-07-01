4220be3c94e96f03bb9062a64f40f7b5d69eebcf
amin509 <365.AMIN.6831@GMAIL.COM>
2024-07-01 09:28:10 +0330

    Add files via upload

---
+++ Nmain.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+
+win=tk.Tk()
+win.geometry("1200x800")
+win.resizable(False,False)
+
+win.config(bg="aqua")
+sovalat=[0,1,2,3,4,5,6,7,8]
+x=0
+def print():
+    # تغیر سوالات
+    a="svalat"
+    lab.config(text=a,font="Homa 10")
+    btn.place(x=500,y=1000)
+    btn2=tk.Button(text="<<",bg="yellow",fg="blue",font="calibri 35")
+    btn2.place(x=480,y=660)
+
+    btn3=tk.Button(text=">>",bg="yellow",fg="blue",font="calibri 35",command=next)
+    btn3.place(x=580,y=660)
+
+def next():
+    x=x+1
+    lab.config(text=sovalat[x])
+    
+    
+
+
+
+
+
+# تغیر سلام
+lab=tk.Label(win,width=100,height=26,font="Homa 10",text="salam")
+lab.place(x=160,y=50)
+str=tk.StringVar
+btn=tk.Button(win,text="start",bg="black",fg="yellow",font="calibri 35",command=print)
+btn.place(x=500,y=660)
+
+win.mainloop()
